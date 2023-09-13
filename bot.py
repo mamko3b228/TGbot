ee0972c62ad24fe77461244955f1e9c61c0849b2
Anton Mogilev <am99270@gmail.com>
2023-09-13 16:55:46 +0300

    gitignore

--- bot.py
+++ bot.py
@@ -15,9 +15,6 @@ from aiogram.dispatcher.filters import Text
 from aiogram.types import CallbackQuery
 from aiogram.utils.callback_data import CallbackData
 
-# # Токен бота
-# API_TOKEN = '6137197627:AAFv8H2MsVKcnqMum7PoyqwK5ltishItAzE'
-
 # Логинг
 logging.basicConfig(level=logging.INFO)
 
@@ -98,67 +95,6 @@ async def process_description(message: types.Message, state: FSMContext):
         data['description'] = message.text
         await Form.description.set()
 
-        # message_data = f"Ваша заявка:\n\nИмя: {data['name']}\n\nНомер телефона: {data['phone']}\n\nОписание: {data['description']}\n\nХотите ли Вы изменить данные?"
-        
-        # keyboard = InlineKeyboardMarkup()
-        # button_yes = InlineKeyboardButton("Да", callback_data="change_data")
-        # button_no = InlineKeyboardButton("Нет", callback_data="no_change")
-        # keyboard.add(button_yes, button_no)            
-                
-        # await message.answer(message_data, reply_markup=keyboard)
-
-        # # Хендлер на изменение данных
-        # @dp.callback_query_handler(text='change_data', state='*')
-        # async def change_data_handler(callback_query: types.CallbackQuery):
-        #     # Создаём клавиатуру для изменения данных
-        #     keyboard = InlineKeyboardMarkup()
-        #     button_change_name = InlineKeyboardButton('Изменить имя', callback_data='change_name')
-        #     button_change_phone = InlineKeyboardButton('Изменить номер', callback_data='change_phone')
-        #     button_change_description = InlineKeyboardButton('Изменить описание', callback_data='change_description')
-        #     keyboard.row(button_change_name)
-        #     keyboard.row(button_change_phone)
-        #     keyboard.row(button_change_description)
-
-        #     # Редактируем сообщение и отправляем клавиатуру
-        #     await callback_query.message.edit_text('Что Вы хотите изменить?', reply_markup=keyboard)
-        
-        #     # Хендлер на изменение имени
-        # @dp.callback_query_handler(text='change_name', state='*')
-        # async def change_name_handler(callback_query: types.CallbackQuery):
-            
-        #     # Создаём клавиатуру для подтверждения изменения
-        #     keyboard = InlineKeyboardMarkup()
-        #     confirm = InlineKeyboardButton('Да', callback_data='confirm_change_name')
-        #     cancel = InlineKeyboardButton('Нет', callback_data='cancel_change_name')
-        #     keyboard.row(confirm)
-        #     keyboard.row(cancel)
-
-        #     # Редактируем сообщение и отправляем клавиатуру
-        #     await callback_query.message.edit_text('Подтверждаете ли Вы изменение имени?', reply_markup=keyboard)
-
-        #     # Устанавливаем новое состояние пользователя
-        #     await Form.name.set()
-
-        # #Хендлер на подтверждение изменения имени
-        # @dp.callback_query_handler(text='confirm_change_name', state=Form.name)
-        # async def confirm_change_name_handler(callback_query: types.CallbackQuery, state: FSMContext):
-        #     # Получаем новое имя из состояния
-        #     data = await state.get_data()
-
-        #     # Отправляем подтверждение изменения
-        #     data['name'] = None
-        #     await state.update_data(name=data['name'])
-
-        #     await callback_query.message.edit_text("Имя изменено: data['name]'", reply_markup=types.ReplyKeyboardRemove())
-        #     await Form.phone.set()
-
-
-    
-
-   
-
-
-
         # Сохранение данных в базе данных SQLite
         conn = sqlite3.connect('database.db')
         cursor = conn.cursor()
@@ -167,33 +103,12 @@ async def process_description(message: types.Message, state: FSMContext):
         conn.commit()
         conn.close()
 
-        # #Send message to user
-        # user_data = f"{data['name']}, Ваша заявка оформлена!\n\nИмя: {data['name']}\n\nНомер телефона: {data['phone']}\n\nОписание: {data['description']}\n\nМы Вам перезвоним!Для оформления новой заявки нажмите /start"           
-        # await message.reply(user_data)
-        # await state.finish()
-
-        # async def send_to_multiple_chats(chats, message_text):
-        #     for chat_id in chats:
-        #         await bot.send_message(chat_id=chat_id, text=message_text)
-        #     # пример использования функции
-        #     chats = [
-        #         5038035009,
-        #         627967659
-        #         ]
-        #     message = f"✅✅✅Новая заявка✅✅✅\n\nИмя: {data['name']}\n\nНомер телефона: {data['phone']}\n\nОписание: {data['description']}"    
-        #     await send_to_multiple_chats(chats, message)
         await bot.send_message(chat_id=ADMIN_ID_DESIGNER, text= f"✅✅✅Новая заявка✅✅✅\n\nИмя: {data['name']}\n\nНомер телефона: {data['phone']}\n\nОписание: {data['description']}")
         
         #Send message to user
         user_data = f"{data['name']}, Ваша заявка оформлена!\n\nИмя: {data['name']}\n\nНомер телефона: {data['phone']}\n\nОписание: {data['description']}\n\nМы Вам перезвоним!Для оформления новой заявки нажмите /start"           
         await message.reply(user_data)
         await state.finish()
-
-    # async def process_description(message: types.Message, state: FSMContext):
-    #     async with state.proxy() as data:
-    #         data['description'] = message.text
-    #         user_data = "\n".join([f"{k}: {v}" for k, v in data.items()])
-    #         await message.reply(f"Спасибо! Мы Вам перезвоним:\n\n{user_data}", parse_mode=ParseMode.MARKDOWN)
 
     # Launch Bot
 if __name__ == '__main__':
